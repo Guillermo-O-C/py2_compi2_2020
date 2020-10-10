b03681a8dc3843516081f76ac7614b6a2ca518cb
Guillermo <josuegorellanac@gmail.com>
2020-10-10 00:49:10 -0600

    Updates

---
+++ precache-manifest.46277786f375119bf3ebd2b809655279.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a9e0503735f9ef6f4f247f541232e18",
+    "url": "/py2_compi2_2020/index.html"
+  },
+  {
+    "revision": "3e28401f32911ea2c034",
+    "url": "/py2_compi2_2020/static/css/main.14f7c604.chunk.css"
+  },
+  {
+    "revision": "1e5126e5a434847d9e3c",
+    "url": "/py2_compi2_2020/static/js/2.e596080f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/py2_compi2_2020/static/js/2.e596080f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e28401f32911ea2c034",
+    "url": "/py2_compi2_2020/static/js/main.ceb7cf1d.chunk.js"
+  },
+  {
+    "revision": "1ada666bc09d61e45c9f",
+    "url": "/py2_compi2_2020/static/js/runtime-main.8acba621.js"
+  }
+]);
