5924147a01fcf85fce2af557bd1b6a2a8b9399fc
Guillermo <josuegorellanac@gmail.com>
2020-10-10 00:42:43 -0600

    primero

---
+++ src/SocialFollow.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SocialFollow() {
+  return (
+    <div class="social-container">
+      <h3 className = "heading">Created by Anjali Sharma</h3>
+    </div>
+  );
+}
